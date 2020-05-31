929e62a090628f7f02a76a05c98d4e2a91e6387f
Rapha-Y <raph.yuki@gmail.com>
2020-05-31 16:07:37 -0300

    added infinite scroll (curso-angular)

--- DIO_Course_AdvAng/curso-angular/src/app/core/filmes.service.ts
+++ DIO_Course_AdvAng/curso-angular/src/app/core/filmes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Filme } from '../shared/models/filme';
 
@@ -16,8 +16,11 @@ export class FilmesService {
     return this.http.post<Filme>(url, filme);
   }
 
-  listar(): Observable<Filme[]> {
-    return this.http.get<Filme[]>(url);
+  listar(pagina: number, qtddPagina: number): Observable<Filme[]> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.set("_page", pagina.toString());
+    httpParams = httpParams.set("_limit", qtddPagina.toString());
+    return this.http.get<Filme[]>(url, {params: httpParams});
   }
 
 }
