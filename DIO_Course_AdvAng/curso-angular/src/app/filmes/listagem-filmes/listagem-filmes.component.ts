ecea8007e74d6f5a10117b08445de90acd385904
Rapha-Y <raph.yuki@gmail.com>
2020-05-31 16:28:06 -0300

    added search fields (curso-angular)

--- DIO_Course_AdvAng/curso-angular/src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ DIO_Course_AdvAng/curso-angular/src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
 
@@ -12,10 +14,19 @@ export class ListagemFilmesComponent implements OnInit {
   filmes: Filme[] = [];
   pagina = 0;
   readonly qtddPagina = 4;
+  filtrosListagem: FormGroup;
+  generos: Array<String>;
 
-  constructor(private filmesService: FilmesService) { }
+  constructor(private filmesService: FilmesService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.filtrosListagem = this.fb.group({
+      texto: [''],
+      genero: ['']
+    });
+
+    this.generos = ["Ação", "Romance", "Aventura", "Terror", "Ficção Científica", "Comédia", "Drama"];
+
     this.listarFilmes();
   }
 
