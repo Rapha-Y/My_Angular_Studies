47d6148a922c064a1a4b81f1cc493c7c2fc28b82
Rapha-Y <raph.yuki@gmail.com>
2020-05-31 22:21:02 -0300

    added the option to delete movies (curso-angular)

--- DIO_Course_AdvAng/curso-angular/src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
+++ DIO_Course_AdvAng/curso-angular/src/app/filmes/visualizar-filmes/visualizar-filmes.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+
 import { FilmesService } from 'src/app/core/filmes.service';
 import { Filme } from 'src/app/shared/models/filme';
+import { Alert } from 'src/app/shared/models/alert';
+import { AlertComponent } from 'src/app/shared/components/alert/alert.component';
 
 @Component({
-  selector: 'visualizar-filmes',
+  selector: 'dio-visualizar-filmes',
   templateUrl: './visualizar-filmes.component.html',
   styleUrls: ['./visualizar-filmes.component.scss']
 })
@@ -12,15 +16,40 @@ export class VisualizarFilmesComponent implements OnInit {
 
   readonly semfoto = "https://www.termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg";
   filme: Filme;
+  id: number;
 
-  constructor(private activedRoute: ActivatedRoute, private filmesService: FilmesService) { }
+  constructor(
+    public dialog: MatDialog,
+    private activedRoute: ActivatedRoute,
+    private router: Router, 
+    private filmesService: FilmesService
+  ) { }
 
   ngOnInit() {
-    this.visualizar(this.activedRoute.snapshot.params['id']);
+    this.id = this.activedRoute.snapshot.params['id'];
+    this.visualizar();
   }
 
-  private visualizar(id: number): void {
-    this.filmesService.visualizar(id).subscribe((filme: Filme) => this.filme = filme);
+  excluir(): void {
+    const config = {
+      data: {
+        titulo: 'Tem certeza que deseja excluir este filme?',
+        descricao: 'Caso tenha certeza que deseja excluir o filme, clique em OK.',
+        corBtnCancelar: 'primary',
+        corBtnSucesso: 'warn',
+        possuiBtnFechar: true
+      } as Alert
+    };
+    const dialogRef = this.dialog.open(AlertComponent, config);
+    dialogRef.afterClosed().subscribe((opcao: boolean) => {
+      if(opcao) {
+        this.filmesService.excluir(this.id).subscribe(() => this.router.navigateByUrl('/filmes'));
+      }
+    });
+  }
+
+  private visualizar(): void {
+    this.filmesService.visualizar(this.id).subscribe((filme: Filme) => this.filme = filme);
   }
 
 }
